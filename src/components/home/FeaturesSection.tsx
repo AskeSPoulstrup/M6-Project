40492622da911af74571010af013b1b67c076b77
AskeSPoulstrup <aske0932@gmail.com>
2026-05-31 10:55:49 +0200

    Importeret billeder og fjernet ae ao oe
    Som forklaret billeder og æ/ø/å

--- src/components/home/FeaturesSection.tsx
+++ src/components/home/FeaturesSection.tsx
@@ -1,7 +1,7 @@
 const features = [
   { icon: 'truck', title: 'Fri fragt til pakkeshop', subtitle: 'Alle ordrer over 599 kr' },
-  { icon: 'phone', title: 'Kundeservice 8-17', subtitle: '+45 70 20 10 10' },
-  { icon: 'pizza', title: 'Stort udvalg', subtitle: 'Raavarer og gear' },
+  { icon: 'phone', title: 'Kundeservice 8-17', subtitle: '+45 12 34 56 78' },
+  { icon: 'pizza', title: 'Stort udvalg', subtitle: 'råvarer og gear' },
 ]
 
 export default function FeaturesSection() {
